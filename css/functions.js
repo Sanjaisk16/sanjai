4ded49c030edebe9673939460511b9e2881ee2c1
sanjai <109976207+Sanjaisk16@users.noreply.github.com>
2023-02-23 21:04:52 +0530

    Add files via upload

---
+++ css/functions.js
@@ -0,0 +1,15 @@
+function greetuser() {
+    let name = "Sanjai";
+    let info = "HI" + name + "Im learning javascript";
+    console.log(info);
+    
+}
+
+function calculator(number1, number2){
+    console.log(number1 * number2);
+}
+
+calculator(5, 5)
+let variable,operators = algorithm;
+
+
